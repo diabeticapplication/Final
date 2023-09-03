286625bad1e9dd0e337aaf43615d536a78a8b3e0
Datingai <applicationdiabetic@gmail.com>
2023-09-03 13:10:19 +0530

    new

--- _data/tools.js
+++ _data/tools.js
@@ -2,7 +2,7 @@
 const EleventyFetch =  require("@11ty/eleventy-fetch");
 
 module.exports = async function () {
-let url = 'https://script.google.com/macros/s/AKfycbxMhxY5Gk2aVXglLp_77xKcqGW49YzK5KOVrMqobfvRV7pebD7flOxz0UJEwe96UjRw/exec';
+let url = 'https://script.google.com/macros/s/AKfycbzb0qL24j5jIGPeu0mOFgsy-599Jy0YAD7w4sARyANOyesi0i5qeX2XNcMAW4PsDsBU/exec';
 
 const response = await EleventyFetch(url, {
 duration: "1d",
